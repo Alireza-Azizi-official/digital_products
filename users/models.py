28415204be130e2626c0de92c9c38e670ea522fe
stranger <alirezaz313313@gmail.com>
2024-06-08 22:12:58 +0330

    .

--- users/models.py
+++ users/models.py
@@ -6,95 +6,81 @@ from django.utils import timezone
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.core.mail import send_mail
 
-
 class UserManager(BaseUserManager):
     use_in_migrations = True
-    
+
     def _create_user(self, username, phone_number, email, password, is_staff, is_superuser, **extra_fields):
         now = timezone.now()
         if not username:
             raise ValueError('The given username must be set')
         email = self.normalize_email(email)
-        user = self.model(phone_number = phone_number,
-                          username = username, email = email, is_staff = is_staff,
-                          is_active = True, is_superuser = is_superuser, date_joined = now,
+        user = self.model(phone_number=phone_number,
+                          username=username, email=email, is_staff=is_staff,
+                          is_active=True, is_superuser=is_superuser, date_joined=now,
                           **extra_fields)
         if not extra_fields.get('no_password'):
             user.set_password(password)
-            
-        user.save(using = self._db)
+
+        user.save(using=self._db)
         return user
-    
-    def create_user(self, username=None, phone_number = None, email = None, password = None, **extra_fields):
+
+    def create_user(self, username=None, phone_number=None, email=None, password=None, **extra_fields):
         if username is None:
             if email:
                 username = email.split('@', 1)[0]
             if phone_number:
                 username = random.choice('qwertyuiopasdfghjklzxcvbnm') + str(phone_number)[-7:]
-            while User.objects.filter(username.randint(10,99)):
+            while User.objects.filter(username__startswith=str(random.randint(10, 99))):
                 username += str(random.randint(10, 99))
-            
+
         return self._create_user(username, phone_number, email, password, False, False, **extra_fields)
-    
+
     def create_superuser(self, username, phone_number, email, password, **extra_fields):
         return self._create_user(username, phone_number, email, password, True, True, **extra_fields)
-    
+
     def get_by_phone_number(self, phone_number):
         return self.get(**{'phone_number': phone_number})
-    
-    
+
 class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(_('username'), max_length= 32, unique=True, 
-                                help_text = _('Required. 30 characters or fewer starting with a letter. Letters, digits'),
-                                validators=[validators.RegexValidator(r'^[a-zA-Z][a-zA-Z0-9_\.]+$ ',
-                                                                      _('Enter a valid username starting with a-z.'
-                                                                        'This value may contain only letters, numbers'
-                                                                        ' and underscore characters.')),],
-                                error_messages={
-                                    'unique':_("A user with that username already exists."),
-                                }
-                                )
+    username = models.CharField(_('username'), max_length=32, unique=True,
+                                help_text=_('Required. 30 characters or fewer starting with a letter. Letters, digits'),
+                                validators=[validators.RegexValidator(r'^[a-zA-Z0-9][a-zA-Z0-9_\.]+$', _('Enter a valid username. This value may contain only letters, numbers, underscore characters, and dots.'), 'invalid')],
+                                error_messages={'unique': _("A user with that username already exists.")})
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=30, blank=True)
-    email = models.EmailField(_('email address'), unique=True, null= True, blank= True)
-    phone_number = models.BigIntegerField(_('mobile number'), unique=True, null= True, blank= True,
-                                          validators=[
-                                              validators.RegexValidator(r'^989[0-3,9]\d{8}$',('ENter a valid mobile number.'))
-                                          ],
-                                          error_messages={
-                                              'unique':_("A user with this mobile number already exists.")
-                                          })
+    email = models.EmailField(_('email address'), unique=True, null=True, blank=True)
+    phone_number = models.BigIntegerField(_('mobile number'), unique=True, null=True, blank=True,
+                                          validators=[validators.RegexValidator(r'^989[0-3,9]\d{8}$', _('Enter a valid mobile number.'))],
+                                          error_messages={'unique': _("A user with this mobile number already exists.")})
     is_staff = models.BooleanField(_('staff status'), default=False,
                                    help_text=_('Designates whether the user can log into this admin site.'))
-    is_active = models.BooleanField(_('active'), default= True,
-                                    help_text=_('Designates whether this user should be treated as active.'
-                                                'Unselect this instead of deleting accounts.'))
-    date_joined = models.DateTimeField(_('date joined'), default= timezone.now)
+    is_active = models.BooleanField(_('active'), default=True,
+                                    help_text=_('Designates whether this user should be treated as active. Unselect this instead of deleting accounts.'))
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
     last_seen = models.DateTimeField(_('last seen date'), null=True)
     objects = UserManager()
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email', 'phone_number']
-    
+
     class Meta:
         db_table = 'users'
         verbose_name = _('user')
         verbose_name_plural = _('users')
-        
-        
+
     def get_full_name(self):
         full_name = '%s %s' % (self.first_name, self.last_name)
         return full_name.strip()
-    
+
     def get_short_name(self):
         return self.first_name
-    
-    def emial_user(self, subject, message, from_email = None, **kwargs):
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
         send_mail(subject, message, from_email, [self.email], **kwargs)
-        
+
     @property
     def is_loggedin_user(self):
         return self.phone_number is not None or self.email is not None
-    
+
     def save(self, *args, **kwargs):
         if self.email is not None and self.email.strip() == '':
             self.email = None
