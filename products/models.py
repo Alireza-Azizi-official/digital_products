9a1fa13b56acad198a4f89374a2cc7a7be6e1da9
stranger <alirezaz313313@gmail.com>
2024-06-08 14:49:23 +0330

    .

--- products/models.py
+++ products/models.py
@@ -48,10 +48,9 @@ class File(models.Model):
         (FILE_VIDEO,_('video')),
         (FILE_PDF,_('pdf'))
     )
-    
-    product = models.ForeignKey('Product', verbose_name=_('product'),related_name = 'files' ,on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', verbose_name=_('product'), related_name = 'files',on_delete=models.CASCADE)
     title = models.CharField(_('title'), max_length=50)
-    file_type = models.PositiveSmallIntegerField(_("file type"), choices = FILE_TYPES) 
+    file_type =models.PositiveSmallIntegerField(_("file type"), choices =FILE_TYPES)
     file = models.FileField(_('file'), upload_to='files/%Y/%m/%d')
     is_enable = models.BooleanField(_('is enable'), default=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
