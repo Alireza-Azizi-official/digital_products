a087160e1044704f031dc4b7805886778683f5de
stranger <alirezaz313313@gmail.com>
2024-06-08 20:33:50 +0330

    .

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _ 
+from .models import User, Province
 
-# Register your models here.
+
+class MyUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'phone_number', 'emial')}),
+        (_('Permissions'), {'fields' :('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes':('wide',),
+            'friends':('username', 'phone_number', 'password1', 'password2'),
+        }),
+    )
